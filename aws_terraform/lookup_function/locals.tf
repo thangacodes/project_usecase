5bd476ac5ebd03760f4cea843e6a58e2e42eb39c
Murugan <thangadurai.murugan@tesco.com>
2023-10-02 21:18:50 +0530

    s3 bucket creation with lower expression used

--- aws_terraform/lookup_function/locals.tf
+++ aws_terraform/lookup_function/locals.tf
@@ -1,8 +1,9 @@
 locals {
   default = {
-    ami_id     = "ami-03d294e37a4820c21"
-    keyname    = "admin"
-    avail_zone = "ap-south-1a"
-    sgp        = ["sg-0fb1052b659369aa8"]
+    ami_id      = "ami-03d294e37a4820c21"
+    keyname     = "admin"
+    avail_zone  = "ap-south-1a"
+    sgp         = ["sg-0fb1052b659369aa8"]
+    bucket_name = "billa-2023-banana-APPLE"
   }
 }
--- aws_terraform/lookup_function/main.tf
+++ aws_terraform/lookup_function/main.tf
@@ -1,8 +1,15 @@
 resource "aws_instance" "vm" {
-  ami                    = local.default.ami_id
-  instance_type          = lookup(var.ins_type, var.user_input, "t2.micro")
-  vpc_security_group_ids = local.default.sgp
-  key_name               = local.default.keyname
-  availability_zone      = local.default.avail_zone
-  tags                   = merge(var.tagging, { Name = "Lookup_VM" })
+  ami                         = local.default.ami_id
+  instance_type               = lookup(var.ins_type, var.user_input, "t2.micro")
+  vpc_security_group_ids      = local.default.sgp
+  key_name                    = local.default.keyname
+  associate_public_ip_address = false
+  availability_zone           = local.default.avail_zone
+  tags                        = merge(var.tagging, { Name = "Lookup_VM" })
+}
+
+## S3 bucket creation
+resource "aws_s3_bucket" "s3" {
+  bucket = lower(local.default.bucket_name)
+  tags   = merge(var.tagging, { Name = lower(local.default.bucket_name) })
 }
