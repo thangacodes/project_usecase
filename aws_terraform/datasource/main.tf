b9e597121b397599006048c85d99415396256ee6
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-19 20:37:43 +0530

    Update main.tf

--- aws_terraform/datasource/main.tf
+++ aws_terraform/datasource/main.tf
@@ -12,5 +12,7 @@ provider "aws" {
 
 ## Data Source: aws_availability_zones
 
+## The Availability Zones data source allows access to the list of AWS Availability Zones which can be accessed by an AWS account within the region configured in the provider.
+
 data "aws_availability_zones" "available" {}
 
