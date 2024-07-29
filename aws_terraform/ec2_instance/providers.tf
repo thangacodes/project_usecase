24a0ddd7eedbcbb4d5ab3cc6ce9c449a0913b234
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-07-29 19:27:43 +0530

    Update providers.tf

--- aws_terraform/ec2_instance/providers.tf
+++ aws_terraform/ec2_instance/providers.tf
@@ -1,3 +1,17 @@
 provider "aws" {
-  region = var.region
+  region                   = var.region
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
 }
+
+## Please note:
+# Authenticating to AWS with Terraform
+# There are many ways to authenticate using the Terraform AWS provider. 
+# Configuration for the AWS Provider can be derived from several sources, which are applied in the following order:
+
+# Parameters in the provider configuration
+# Environment variables
+# Shared credentials and configuration files
+# Container credentials
+# Instance profile credentials and region
+# External credentials process
