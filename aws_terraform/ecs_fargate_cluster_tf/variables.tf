c05834363dc6e8d09de2be18b71106316c124259
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-08-09 08:19:32 +0530

    initial commit

---
+++ aws_terraform/ecs_fargate_cluster_tf/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "tagging" {
+  default = {
+    CreationDate = "08/08/2024"
+    Environment  = "Development"
+    Project      = "Cloudbird"
+    Owner        = "thangadurai.murugan@cloudbird.fun"
+  }
+}
