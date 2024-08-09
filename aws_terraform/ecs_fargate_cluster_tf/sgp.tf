d286225351fd67ee8d930a9928c7003aabbc8f8c
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-08-09 08:18:58 +0530

    initial commit

---
+++ aws_terraform/ecs_fargate_cluster_tf/sgp.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "SG" {
+  name        = "SG"
+  description = "Allow port 80"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    name = "SG"
+  }
+}
