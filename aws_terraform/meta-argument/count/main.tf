8a4e17e6f304b0863af4d6d5e1db54a196e33f36
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-08-10 06:35:12 +0530

    updated file

--- aws_terraform/meta-argument/count/main.tf
+++ aws_terraform/meta-argument/count/main.tf
@@ -1,4 +1,5 @@
 locals {
+  region               = "ap-south-1"
   availability_zones   = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
   vpc_cidr             = "200.0.0.0/16"
   private_subnets_cidr = ["200.0.0.0/24", "200.0.1.0/24", "200.0.2.0/24"]
