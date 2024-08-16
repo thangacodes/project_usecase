73d1e5d719592bceccfed983c0afaf1f6497a612
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-08-16 08:06:17 +0530

    initial commit

---
+++ aws_terraform_json/simple_ec2/outputs.tf
@@ -0,0 +1,10 @@
+{
+    "output": {
+      "instance_public_ip": {
+        "value": "${aws_instance.web_instance.public_ip}"
+    },
+    "instance_private_ip": {
+        "value": "${aws_instance.web_instance.private_ip}"
+    }
+}
+}
