32a57209609935b1cbc9f3724bcd700567a5aa38
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-10-20 17:35:07 +0530

    Update variables.tf

--- aws_terraform/default_tag/variables.tf
+++ aws_terraform/default_tag/variables.tf
@@ -14,3 +14,23 @@ variable "aws_secret_key" {
   description = "AWS API SECRET_ACCESS_KEY"
   default     = "***********************************"
 }
+
+variable "ami_id" {
+  default     = "ami-04a37924ffe27da53"
+  description = "Image to provision EC2 Machine in AWS Cloud"
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "Type that we provision as EC2 Machine in AWS Cloud"
+}
+
+variable "sgp" {
+  default     = ["sg-0fb1052b659369aa8"]
+  description = "Virtual Firewall for EC2 machine"
+}
+
+variable "sshkey" {
+  default     = "mac"
+  description = "Virutally connect remote machine via SSH key"
+}
